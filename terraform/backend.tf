980fb4a93f32c4ec1168892e13b0a709de5daac9
luqmannnn <luqmannurhakimt@gmail.com>
2024-10-29 18:34:04 +0800

    Fix issues found

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sctp-ce7-tfstate" 
+    bucket = "sctp-ce7-tfstate"
     key    = "terraform-simple-cicd-action-luqman.tfstate"
     region = "us-east-1"
   }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "luqman-simple-cicd-bucket-12345678"
+  #checkov:skip=CKV_AWS_18:Ensure the S3 bucket has access logging enabled
+  #checkov:skip=CKV2_AWS_61:Ensure that an S3 bucket has a lifecycle configuration
+  #checkov:skip=CKV2_AWS_62:Ensure S3 buckets should have event notifications enabled
+  #checkov:skip=CKV_AWS_145:Ensure that S3 buckets are encrypted with KMS by default
+  #checkov:skip=CKV2_AWS_6:Ensure that S3 bucket has a Public Access block
+  #checkov:skip=CKV_AWS_144:Ensure that S3 bucket has cross-region replication enabled
+  #checkov:skip=CKV_AWS_21:Ensure all data stored in the S3 bucket have versioning enabled
+  bucket        = "luqman-simple-cicd-bucket-12345678"
+  force_destroy = true
+  acl           = "public-read"
 }
